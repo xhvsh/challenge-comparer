422136a88e612d5b7db382553df6035ea67416dc
xhvsh <denkiewicz.janek@gmail.com>
2024-05-20 21:11:16 +0200

    update, solved issues, fixed ui, faster data fetching

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let popupOpen = false
+function toast(str) {
+  if (!popupOpen) {
+    popupOpen = true
+    document.querySelector('.toast').classList.remove('hidden')
+    document.querySelector('.msg').innerHTML = str
+    setTimeout(() => {
+      document.querySelector('.toast').classList.add('hidden')
+      popupOpen = false
+    }, 5000)
+  } else {
+    return
+  }
+}
+
 async function getChallenge(challenge) {
   const response = await fetch(`https://data.ninjakiwi.com/btd6/challenges/challenge/${challenge}`)
   const data = await response.json()
@@ -8,7 +23,7 @@ async function getChallenge(challenge) {
 
   if (body.removeableCostMultiplier === -1) body.removeableCostMultiplier = 1
   body._towers = body?._towers?.filter((x) => x?.max !== 0)
-  body?._towers?.sort((a, b) => (a?.tower?.toUpperCase() < b?.tower?.toUpperCase() ? -1 : a?.tower?.toUpperCase() > b?.tower?.toUpperCase() ? 1 : 0))
+  body?._towers?.sort((a, b) => a?.tower?.toUpperCase().localeCompare(b?.tower?.toUpperCase()))
 
   body.allCamo = body?._bloonModifiers.allCamo
   body.allRegen = body?._bloonModifiers.allRegen
@@ -28,30 +43,15 @@ async function getChallenge(challenge) {
 function clean(challenge) {
   const body = challenge
 
-  delete body?.name
-  delete body?.createdAt
-  delete body?.id
-  delete body?.creator
-  delete body?.gameVersion
-  delete body?.mapURL
-  delete body?.disableDoubleCash
-  delete body?.disableInstas
-  delete body?.disablePowers
+  const keysToDelete = ['name', 'createdAt', 'id', 'creator', 'gameVersion', 'mapURL', 'disableDoubleCash', 'disableInstas', 'disablePowers', 'noContinues', 'plays', 'wins', 'restarts', 'losses', 'upvotes', 'playsUnique', 'winsUnique', 'lossesUnique', '_powers', '_bloonModifiers', '_towers']
+
+  keysToDelete.forEach((key) => delete body?.[key])
+
   if (body?.roundSets.length === 1 && body?.roundSets[0] === 'default') delete body?.roundSets
-  delete body?.noContinues
-  delete body?.plays
-  delete body?.wins
-  delete body?.restarts
-  delete body?.losses
-  delete body?.upvotes
-  delete body?.playsUnique
-  delete body?.winsUnique
-  delete body?.lossesUnique
-  delete body?._powers
-  delete body?._bloonModifiers?.bossSpeedMultiplier
-  delete body?._bloonModifiers?.healthMultipliers?.boss
-  delete body?._bloonModifiers
-  delete body?._towers
+  if (body?._bloonModifiers) {
+    delete body._bloonModifiers.bossSpeedMultiplier
+    delete body._bloonModifiers.healthMultipliers?.boss
+  }
 
   return body
 }
@@ -67,55 +67,71 @@ function fixMax(max) {
 }
 
 async function main() {
-  if (document.querySelectorAll('input')[0].value.length < 7 || document.querySelectorAll('input')[1].value.length < 7) {
-    alert('Enter two valid codes please.')
+  document.querySelector('.fade').classList.remove('hidden')
+
+  const inputs = document.querySelectorAll('input')
+  if (inputs[0].value.length < 7 || inputs[1].value.length < 7) {
+    document.querySelector('.fade').classList.add('hidden')
     return
   }
+  if (inputs[0].value.length == 7) inputs[0].value = inputs[0].value.toUpperCase()
+  if (inputs[1].value.length == 7) inputs[1].value = inputs[1].value.toUpperCase()
 
-  const ch1 = clean(await getChallenge(document.querySelectorAll('input')[0].value))
-  const ch2 = clean(await getChallenge(document.querySelectorAll('input')[1].value))
+  const [ch1, ch2] = await Promise.all([getChallenge(inputs[0].value), getChallenge(inputs[1].value)])
 
   if (!ch1 || !ch2) {
-    alert('Enter two valid codes please.')
+    document.querySelector('.fade').classList.add('hidden')
+    toast('Enter two valid codes please.')
     return
   }
 
-  if (JSON.stringify(ch1) === JSON.stringify(ch2)) {
-    alert('There are no differences in this challenges.')
-  } else {
-    document.querySelector('table').innerHTML = `
+  const cleanedCh1 = clean(ch1)
+  const cleanedCh2 = clean(ch2)
+
+  if (JSON.stringify(cleanedCh1) === JSON.stringify(cleanedCh2)) {
+    document.querySelector('.fade').classList.add('hidden')
+    toast('There are no differences in this challenges.')
+    return
+  }
+
+  const table = document.querySelector('table')
+  table.innerHTML = `
     <tr>
       <th>Difference</th>
       <th>First Challenge</th>
       <th>Second Challenge</th>
     </tr>
-    `
-    document.querySelector('.content').classList.add('hidden')
-    document.querySelector('.differences').classList.remove('hidden')
-    const ch1keys = Object.keys(ch1)
-    const ch2keys = Object.keys(ch2)
-    const allkeys = new Set([...ch1keys, ...ch2keys])
+  `
 
-    for (const key of allkeys) {
-      if (ch1[key] !== ch2[key]) {
-        document.querySelector('table').innerHTML += `
-          <tr>
-            <td>${key}</td>
-            <td>${ch1[key]}</td>
-            <td>${ch2[key]}</td>
-          </tr>
-        `
-      }
+  document.querySelector('.fade').classList.add('hidden')
+  document.querySelector('.content').classList.add('hidden')
+  document.querySelector('.differences').classList.remove('hidden')
+
+  const fragment = document.createDocumentFragment()
+  const ch1keys = Object.keys(cleanedCh1)
+  const ch2keys = Object.keys(cleanedCh2)
+  const allkeys = new Set([...ch1keys, ...ch2keys])
+
+  for (const key of allkeys) {
+    if (cleanedCh1[key] !== cleanedCh2[key]) {
+      const row = document.createElement('tr')
+
+      row.innerHTML = `
+        <td>${key.replace(/([A-Z])/g, ' $1').replace('M K', 'MK')}</td>
+        <td>${String(cleanedCh1[key]).replace(/([A-Z])/g, ' $1')}</td>
+        <td>${String(cleanedCh2[key]).replace(/([A-Z])/g, ' $1')}</td>
+      `
+      fragment.appendChild(row)
     }
   }
+
+  table.appendChild(fragment)
 }
 
 document.querySelector('.goback').addEventListener('click', () => {
   document.querySelector('.content').classList.remove('hidden')
   document.querySelector('.differences').classList.add('hidden')
-  document.querySelectorAll('tr').forEach((e) => {
-    e.remove
-  })
+  document.querySelector('table').innerHTML = ''
   document.querySelectorAll('input').forEach((e) => {
     e.value = ''
   })
